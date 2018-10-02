bdbc63f70044a9f46c4b63d4e3625f49144b7a86
Daniël de Kok <me@danieldk.eu>
2018-10-02 11:38:40 +0200

    Add Python utility to create (just) the parsing graph.

---
+++ dpar-utils/tensorflow-rust-train/config.py
@@ -0,0 +1,9 @@
+class DefaultConfig:
+    init_scale = 0.05
+    hidden_size = 200
+    morph_hidden_size = 50
+    deprel_embed_size = 50
+    keep_prob = 0.95
+    keep_prob_input = 0.90
+    num_layers = 2
+    lr_decay = 0.8
